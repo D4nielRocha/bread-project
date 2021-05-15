3552e6f946348c24c592e5070d7f7f5da983ed22
Daniel Rocha <x00170715@mytudublin.ie>
2021-05-15 03:49:47 +0100

    reestructuring + flash

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 const AppError = require('./utils/AppError');
-const router = require('express').Router();
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+const cookieSginature = require('cookie-signature');
+const session = require('express-session');
+const flash = require('connect-flash');
 require('dotenv').config();
 
 
@@ -16,15 +20,36 @@ require('dotenv').config();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname , 'public')));
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(cookieParser('mysecret'));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000* 60 * 60 * 24 * 7,
+        maxAge: 1000* 60 * 60 * 24 * 7,
+    }
+}))
+app.use(flash());
 // app.use(morgan("dev"));
+app.use( (req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next();
+})
 
-app.use('/', require('./routes/bread.js'))
+app.use('/bread', require('./routes/bread.js'))
+app.use('/bread/:id/review', require('./routes/review.js'))
 
-
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 //404 handler
 app.all('*', (req, res, next) => {
@@ -32,12 +57,13 @@ app.all('*', (req, res, next) => {
 })
 
 
+
+
 //error handler
 app.use((err, req, res, next) => {
-    const { status = 500, message = 'something went wrong'} = err;
-    // if(!err.message) err.message = "Something went wrong"
-    res.status(status).render('404', {err});
-    // res.send("Oh Bow, something went wrong!");
+    const {status = 500} = err;
+    if(!err.message) err.message = 'Something went wrong!';
+    res.status(status).render('404', {err,});
 })
 
 
--- routes/bread.js
+++ routes/bread.js
@@ -5,88 +5,75 @@ const Review = require('../models/review');
 const wrapAsync = require('../utils/wrapAsync');
 const { mongoose, db } = require('../db/mongodb');
 const AppError = require('../utils/AppError');
+const flash = require('connect-flash');
+
 
 
 
 //Bread Routes
-router.get('/', (req, res) => {
-    res.render('home')
-})
 
-router.get('/bread', async (req, res) => {
+router.get('/', wrapAsync( async (req, res) => {
     const bread = await Bread.find();
-    res.render('bread/index', {bread});
-})
+    res.render('bread/index', {bread });
+}))
 
-router.get('/bread/new', (req, res) => {
+router.get('/new', (req, res) => {
     res.render('bread/new')
 })
 
 
-router.post('/bread', validateBread, wrapAsync(async (req, res, next) => {    
+router.post('/', validateBread, wrapAsync(async (req, res, next) => {    
         const bread = new Bread(req.body.bread);
         await bread.save();
+        req.flash('success', "New Bread Created Successfully");
         res.redirect(`/bread/${bread._id}`);
 }))
 
 
 
-router.get('/bread/:id', wrapAsync(async(req, res, next) => {
+router.get('/:id', wrapAsync(async(req, res, next) => {
     const bread = await Bread.findById(req.params.id).populate('reviews')
     // console.log(bread);
     if(!bread){
-        throw new AppError("This Post doesn`t exist...Yet", 500);
+        req.flash('error', 'Cannot find this Post');
+        return res.redirect('/bread');
+        // throw new AppError("This Post doesn`t exist...Yet", 500);
     }
-    res.render('bread/show', {bread})
+    // res.render('bread/show', {bread, msg: req.flash('success')});
+    res.render('bread/show', { bread });
 }))
 
 
 
 
-router.get('/bread/:id/edit', wrapAsync(async(req, res, next) => {
+router.get('/:id/edit', wrapAsync(async(req, res, next) => {
     const bread = await Bread.findById(req.params.id);
     if(!bread){
-        throw new AppError("Something went wrong with this post", 400);
+        req.flash('error', 'Cannot find this Post');
+        return res.redirect('/bread');
     }
     res.render('bread/edit', {bread})
 }))
 
 
 
-router.put('/bread/:id', validateBread, wrapAsync(async (req, res, next) => {
+router.put('/:id', validateBread, wrapAsync(async (req, res, next) => {
   
     const {id} = req.params;
     const bread = await Bread.findByIdAndUpdate(id, {...req.body.bread}, {runValidators: true})
-    if(!bread){
-        throw new AppError('Something went wrong updataing this post', 400 )
-    }
+    req.flash('success', 'Successfully updated Bread Post');
     res.redirect(`/bread/${bread._id}`)
 }))
 
-router.delete('/bread/:id', async (req, res) => {
+router.delete('/:id', wrapAsync(async (req, res) => {
     const {id} = req.params;
     const bread = await Bread.findByIdAndDelete(id);
+    req.flash('error', 'Deleted Bread Post');
     res.redirect('/bread');
-})
-
-
-//review routes
-
-router.post('/bread/:id/review', validateReview,  wrapAsync(async (req, res) => {
-    const bread = await Bread.findById(req.params.id);
-    const review = new Review(req.body.review);
-    bread.reviews.push(review);
-    await bread.save();
-    await review.save();
-    res.redirect(`/bread/${bread._id}`);
-}));
-
-router.delete('/bread/:id/review/:reviewId', wrapAsync(async(req, res) => {
-    const {id, reviewId} = req.params;
-    const bread = await Bread.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    const review = await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/bread/${id}`);
 }))
+
+
+
 
 
 module.exports = router;
