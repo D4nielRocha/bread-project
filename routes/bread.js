2670390811f0e14ad039ea9a37b0997968ea8ca2
Daniel Rocha <x00170715@mytudublin.ie>
2021-05-19 00:14:28 +0100

    2nd commit

--- routes/bread.js
+++ routes/bread.js
@@ -5,7 +5,36 @@ const Review = require('../models/review');
 const wrapAsync = require('../utils/wrapAsync');
 const { mongoose, db } = require('../db/mongodb');
 const AppError = require('../utils/AppError');
+const bcrypt = require('bcrypt');
 const flash = require('connect-flash');
+
+
+// const hashPassword = async(password) => {
+//     const salt = await bcrypt.genSalt(12);
+//     const hash = await bcrypt.hash(password, salt);
+//     console.log(salt);
+//     console.log(hash);
+// }
+
+
+const hashPassword = async(password) => {
+    const hash = await bcrypt.hash(password, 12);
+    // console.log(salt);
+    console.log(hash);
+}
+
+ const login = async (password, hashedPw) => {
+    const result = await bcrypt.compare(password, hashedPw);
+    if(result){
+        console.log('Logged you in successfully', result);
+    } else {
+        console.log('Incorrect password!!');
+    }
+ }
+
+// hashPassword('daniel88');
+login('daniell88', "$2b$12$S9Na4SaqlUIgJBpoqg1L.eUlLDmyEPFyrOQV7MWGZbTqUmRMy7fxe")
+
 
 
 
